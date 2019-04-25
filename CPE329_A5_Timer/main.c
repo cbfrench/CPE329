fbf0e5137bedf41c1698f4509186b2560a09616e
mrscience21 <mrscience21@gmail.com>
2019-04-24 21:22:34 -0700

    Light Modifications working on A5 part (a)
    Added comments and tried modifying the given timer lengths for desired results.

--- CPE329_A5_Timer/main.c
+++ CPE329_A5_Timer/main.c
@@ -2,8 +2,8 @@
 #include "delay.h"
 
 
-#define LENGTH 1920
-#define SHORT_LENGTH 640
+#define LENGTH 3//960
+#define SHORT_LENGTH 1//240
 
 #define OUTPUT P5
 
@@ -31,15 +31,25 @@ void main(void)
 
 	set_DCO(FREQ_24_MHz);
 
+	// Set-Up P4.3 to output MCLK Signal
+    P4 -> DIR |= BIT3;                                  // Direction: Output
+    P4 -> DS &= BIT3;                                   // Regular Output Power
+    P4 -> SEL0 |= BIT3;                                 // Set for Primary Output
+    P4 -> SEL1 &= BIT3;
+
+
 	OUTPUT->SEL0 &= ~(BIT7);
 	OUTPUT->SEL1 &= ~(BIT7);
 	OUTPUT->DIR |= BIT7;
 
 	//set output to be measured on oscilloscope
-	TIMER_A0->CTL |= TIMER_A_CTL_SSEL__SMCLK | TIMER_A_CTL_MC_2 | TIMER_A_CTL_IE;
-	TIMER_A0->CCR[0] = SHORT_LENGTH;
+	TIMER_A0->CTL |= TIMER_A_CTL_SSEL__SMCLK        // Use SMCLK
+	              | TIMER_A_CTL_MC_2;               // Use Continuous Mode
+
+	TIMER_A0->CCR[0] = SHORT_LENGTH;                // Set CCR0 and CCR1 lengths
 	TIMER_A0->CCR[1] = LENGTH;
-	TIMER_A0->CCTL[0] |= TIMER_A_CCTLN_CCIE;
+
+	TIMER_A0->CCTL[0] |= TIMER_A_CCTLN_CCIE;        // Enable Interrupts
 	TIMER_A0->CCTL[1] |= TIMER_A_CCTLN_CCIE;
 	NVIC->ISER[0] = 1 << (TA0_0_IRQn & 31);
 	NVIC->ISER[0] = 1 << (TA0_N_IRQn & 31);
