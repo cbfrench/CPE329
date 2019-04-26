d2353a136d75bdb63e400f70cab644e16acb1594
mrscience21 <mrscience21@gmail.com>
2019-04-25 23:37:54 -0700

    Completed Assignment 5
    Working Software for A5 Part E

--- CPE329_A5_Timer/main.c
+++ CPE329_A5_Timer/main.c
@@ -2,8 +2,8 @@
 #include "delay.h"
 
 // CURRENTLY CONFIGURED FOR A5 PART C
-#define LENGTH 5000000
-#define LONGER_LENGTH 10000000
+#define LENGTH 750
+#define LONGER_LENGTH 1500
 
 #define OUTPUT P5
 
@@ -48,16 +48,15 @@ void main(void)
 
 	//set output to be measured on oscilloscope
 	TIMER_A0->CTL |= TIMER_A_CTL_SSEL__SMCLK | TIMER_A_CTL_MC_2 | TIMER_A_CTL_ID_0;
-	TIMER_A0->CTL |= TIMER_A_CTL_MC_2 | TIMER_A_CTL_ID_0;
 
 	TIMER_A0->CCR[0] = LENGTH;                          // Set CCR0 and CCR1 lengths
 	TIMER_A0->CCR[1] = LONGER_LENGTH;
 
-	TIMER_A0->CCTL[0] |= TIMER_A_CCTLN_CCIE;            // Enable Interrupts
-	TIMER_A1->CCTL[1] |= TIMER_A_CCTLN_CCIE;
+	TIMER_A0->CCTL[0] |= TIMER_A_CCTLN_CCIE;        // Enable Interrupts
+    TIMER_A0->CCTL[1] |= TIMER_A_CCTLN_CCIE;
 
-	NVIC->ISER[0] = 1 << (TA0_0_IRQn & 31);
-	NVIC->ISER[0] |= 1 << (TA0_N_IRQn & 31);
+    NVIC->ISER[0] = 1 << (TA0_0_IRQn & 31);
+    NVIC->ISER[0] = 1 << (TA0_N_IRQn & 31);
 	__enable_irq();
 	while(1){
 	}
