5b24646f6f437c218e221af670b62dfe895565d3
Connor French <cbfrench@calpoly.edu>
2019-05-13 22:52:25 -0700

    Waiting on UART:

--- CPE329_A9_ADC/main.c
+++ CPE329_A9_ADC/main.c
@@ -2,23 +2,141 @@
 #include "delay.h"
 #include "dac.h"
 
+int adc_flag = 0;
+volatile uint16_t readValue;
+volatile double writeValue;
+
+int flag = 0;
+char inValue[5];
+int index = 0;
+int voltageOut = 0;
+
+void EUSCIA0_IRQHandler(void) {
+    char letter;
+    //P2->OUT |= BIT2;
+    if (EUSCI_A0->IFG & EUSCI_A_IFG_RXIFG) {
+        // Cancel Interrupt Flag
+        EUSCI_A0->IFG &= ~EUSCI_A_IFG_RXIFG;
+
+        // Process ASCII Character received by UART into a character array
+        letter = EUSCI_A0->RXBUF;
+        while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
+        EUSCI_A0->TXBUF = letter;
+        if (letter >= '0' && letter <= '9' && index < 4) {
+            inValue[index] = letter;
+            index++;
+        }
+        if (letter == 0xD) {
+            // If carriage return is detected, cancel read flag
+            inValue[index] = '\0';
+            flag = 1;
+        }
+    }
+    //P2->OUT &= ~BIT2;
+}
+
+/**
+ * Initialize UART Communications
+ */
+void init_UART() {
+    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST;         // Enable Software Reset
+    EUSCI_A0->CTLW0 =   EUSCI_A_CTLW0_PEN           // Parity (Odd)
+                      | EUSCI_A_CTLW0_SPB           // 2 Stop Bits
+                      | EUSCI_A_CTLW0_MODE_0        // UART mode
+                      | EUSCI_A_CTLW0_SSEL__SMCLK   // Use SMCLK
+                      | EUSCI_A_CTLW0_SWRST;        // Keep in Software Reset
+
+    EUSCI_A0->BRW = 0x01;                           // From UCBRx Calculation
+    EUSCI_A0->MCTLW = (10 << EUSCI_A_MCTLW_BRF_OFS) // Need to shift 4 bits
+                      | EUSCI_A_MCTLW_OS16;         // Increase Sensitivity
+
+    P1->SEL0 |= (BIT2 | BIT3);                      // Select EUSCI_A0 Tx and Rx
+    P1->SEL1 &= ~(BIT2 | BIT3);
+
+    EUSCI_A0->CTLW0 &= ~EUSCI_A_CTLW0_SWRST;        // Disable Software Reset
+
+    EUSCI_A0->IFG &= ~EUSCI_A_IFG_RXIFG;            // Clear and Enable Rx Interrupt Flags
+    EUSCI_A0->IE |= EUSCI_A_IE_RXIE;
+}
+
+void print_char(char letter) {
+    while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
+    EUSCI_A0->TXBUF = letter;
+
+    //P2->OUT ^=BIT2;
+}
+
+/**
+ * Outputs String Characters over UART
+ */
+void print_string(char* string) {
+    int i;
+    for (i = 0; i < strlen(string); i++)
+        print_char(string[i]);
+}
+
+void check_UART(){
+    if (flag) {
+        print_string(inValue);                  // Return Value sent over UART
+        voltageOut = atoi(inValue);                // Convert sent value to Integer
+        if (voltageOut < 4096 && voltageOut >= 0)
+            set_voltage(voltageOut);               // Set Voltage if within allowed range
+        flag = 0;                               // Clear Flags and temp. variables
+        index = 0;
+        voltageOut = 0;
+    }
+}
+
 void init_ADC(){
-    P6->SEL0 |= BIT1;
-    P6->SEL1 |= BIT1;
     ADC14->CTL0 &= ~ADC14_CTL0_ENC;
     ADC14->CTL0 = ADC14_CTL0_SHP
-            | ADC14_CTL0_SSEL_2
+            | ADC14_CTL0_SSEL_4
+            | ADC14_CTL0_SHT1__16
             | ADC14_CTL0_ON;
-    ADC14->CTL1 = (0 << ADC14_CTL1_CSTARTADD_OFS)
+    ADC14->CTL1 = (14 << ADC14_CTL1_CSTARTADD_OFS)
             | ADC14_CTL1_RES_3;
-    ADC14->MCTL[0] = ADC14_MCTLN_INCH_0;
-    ADC14->IER0 = ADC14_IER0_IE0;
+    ADC14->MCTL[14] = ADC14_MCTLN_INCH_14;
+    ADC14->IER0 |= ADC14_IER0_IE14;
     ADC14->CTL0 |= ADC14_CTL0_ENC;
 }
 
 void ADC14_IRQHandler(void){
-    volatile uint8_t readValue;
-    readValue = ADC14->MEM[0];
+    readValue = ADC14->MEM[14];
+    adc_flag = 1;
+}
+
+void ADC_4_cycles(){
+    writeValue = 0.0002 * readValue + 0.0185;
+}
+
+void ADC_16_cycles(){
+    writeValue = 0.0002 * readValue + 0.0001;
+}
+
+void ADC_96_cycles(){
+    writeValue = 0.0002 * readValue + 0.0008;
+}
+
+void ADC_192_cycles(){
+    writeValue = 0.0002 * readValue + 0.0024;
+}
+
+void print_float(float input){
+    float f = input;
+    int count = 0;
+    int round;
+    while(f != 0 && count < 5){
+        if(count == 1){
+            print_char('.');
+        }
+        else{
+            round = (int)f;
+            print_char(round + '0');
+            f = (f - round) * 10;
+        }
+        count++;
+    }
+    print_char('\n');
 }
 
 void main(void)
@@ -26,11 +144,26 @@ void main(void)
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 	set_DCO(FREQ_24_MHz);
 
+	init_UART();
 	init_ADC();
-	NVIC->ISER[0] = (1 << ADC14_IRQn & 31);
+
+	NVIC->ISER[0] = (1 << (ADC14_IRQn & 31));
+	NVIC->ISER[1] = (1 << (EUSCIA0_IRQn & 31));
+
 	__enable_irq();
+
+    P6->SEL0 |= BIT1;
+    P6->SEL1 |= BIT1;
+
+	ADC14->CTL0 |= ADC14_CTL0_SC;
 	while(1){
-	    ADC14->CTL0 |= ADC14_CTL0_SC;
+	    if(adc_flag){
+	        adc_flag = 0;
+	        ADC_16_cycles();
+	        print_char('h');
+	        //print_float(writeValue);
+	        ADC14->CTL0 |= ADC14_CTL0_SC;
+	    }
 	    delay_us(1000);
 	}
 }
